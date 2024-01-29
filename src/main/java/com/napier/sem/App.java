6723d96ef744da6cc8d65037120b1340ded42c20
seinlei <seimleihtarni@gmail.com>
2024-01-29 20:31:13 +0630

    fixed error
    Signed-off-by: seinlei <seimleihtarni@gmail.com>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,26 +5,28 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         // Connect to MongoDB
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Sein Lei")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2024")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
+        try (MongoClient mongoClient = new MongoClient("mongo-dbserver")) {
+            // Get a database - will create when we use it
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
+            // Create a document to store
+            Document doc = new Document("name", "Sein Lei")
+                    .append("class", "Software Engineering Methods")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+            // Add document to collection
+            collection.insertOne(doc);
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            System.out.println(myDoc.toJson());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
