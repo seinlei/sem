7605f3503a9c788f34f28a19e3a4f611b10b1e9d
seinlei <seimleihtarni@gmail.com>
2024-02-12 22:16:33 +0630

    Connecting database successful

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,78 +6,57 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Create new Application
-        App a = new App();
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
 
-        // Connect to database
-        a.connect();
+        // Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                // Wait a bit
+                Thread.sleep(10000);
+                // Exit for loop
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
 
-        // Disconnect from database
-        a.disconnect();
-    }
-
-        /**
-         * Connection to MySQL database.
-         */
-        private Connection con = null;
-
-        /**
-         * Connect to the MySQL database.
-         */
-        public void connect()
+        if (con != null)
         {
             try
             {
-                // Load Database driver
-                Class.forName("com.mysql.cj.jdbc.Driver");
+                // Close connectionS
+                con.close();
             }
-            catch (ClassNotFoundException e)
+            catch (Exception e)
             {
-                System.out.println("Could not load SQL driver");
-                System.exit(-1);
-            }
-
-            int retries = 10;
-            for (int i = 0; i < retries; ++i)
-            {
-                System.out.println("Connecting to database...");
-                try
-                {
-                    // Wait a bit for db to start
-                    Thread.sleep(30000);
-                    // Connect to database
-                    con = DriverManager.getConnection("jdbc:mysql://db:33060/employees?useSSL=false", "root", "example");
-                    System.out.println("Successfully connected");
-                    break;
-                }
-                catch (SQLException sqle)
-                {
-                    System.out.println("Failed to connect to database attempt " + i);
-                    System.out.println(sqle.getMessage());
-                }
-                catch (InterruptedException ie)
-                {
-                    System.out.println("Thread interrupted? Should not happen.");
-                }
+                System.out.println("Error closing connection to database");
             }
         }
-
-        /**
-         * Disconnect from the MySQL database.
-         */
-        public void disconnect()
-        {
-            if (con != null)
-            {
-                try
-                {
-                    // Close connection
-                    con.close();
-                }
-                catch (Exception e)
-                {
-                    System.out.println("Error closing connection to database");
-                }
-            }
-        }
+    }
 }
